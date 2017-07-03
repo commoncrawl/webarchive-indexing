377d72c05d27727c2ce91244880c9f61378e6568
Sebastian Nagel <sebastian@commoncrawl.org>
2017-07-03 16:45:45 +0200

    Improved script to publish index - allow to perform publishing without interaction - remove intermediate files

--- publish_index.sh
+++ publish_index.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 YEARWEEK="$1"
+MONTH="$2"
 
 if [ -z "$YEARWEEK" ]; then
-    echo "$0 <year-week-of-crawl-archive>"
-    exit 1
+	echo "$0 <year-week-of-crawl-archive> [<month>]"
+	exit 1
 fi
 
 set -x
@@ -13,9 +14,14 @@ set -e
 
 ## Create the metadata (title) for the index on the website
 if ! [ -e $YEARWEEK-metadata.yaml ]; then
-    s3cmd get s3://commoncrawl/cc-index/collections/CC-MAIN-2015-18/metadata.yaml $YEARWEEK-metadata.yaml
-    echo "Please, edit $YEARWEEK-metadata.yaml"
-    exit 1
+	if [ -n "$MONTH" ]; then
+		YEAR=${YEARWEEK%%-*}
+		echo "title: '${MONTH^} $YEAR Index'" >$YEARWEEK-metadata.yaml
+	else
+		s3cmd get s3://commoncrawl/cc-index/collections/CC-MAIN-2015-18/metadata.yaml $YEARWEEK-metadata.yaml
+		echo "Please, edit $YEARWEEK-metadata.yaml"
+		exit 1
+	fi
 fi
 s3cmd put $YEARWEEK-metadata.yaml s3://commoncrawl/cc-index/collections/CC-MAIN-$YEARWEEK/metadata.yaml --acl-public
 
@@ -27,12 +33,17 @@ s3cmd get --force s3://commoncrawl/cc-index/collections/CC-MAIN-$YEARWEEK/indexe
 cat part-* | awk '{printf "%s\t%s\n",$0,NR}' > cluster.idx
 export LC_ALL=C
 sort -c ./cluster.idx
-rm ./part-00*
+#rm ./part-00*
 s3cmd put ./cluster.idx s3://commoncrawl/cc-index/collections/CC-MAIN-$YEARWEEK/indexes/cluster.idx --acl-public
 
 ## set public permissions where needed (technically only the cdx-* need to be public)
-s3cmd setacl s3://commoncrawl/cc-index/collections/CC-MAIN-$YEARWEEK/indexes/ --acl-private --recursive --exclude='*' --include='part-*' --include='_SUCCESS'
 s3cmd setacl s3://commoncrawl/cc-index/collections/CC-MAIN-$YEARWEEK/indexes/ --acl-public --recursive --exclude='*' --include='cdx-*.gz' --include='cluster.idx' --include='metadata.yaml'
 
+# remove obsolete data from bucket
+#  - map-reduce _SUCCESS file/marker
+aws s3 rm s3://commoncrawl/cc-index/collections/CC-MAIN-$YEARWEEK/indexes/_SUCCESS
+#  - part-00* files concatenated to cluster.idx
+aws s3 rm --recursive s3://commoncrawl/cc-index/collections/CC-MAIN-$YEARWEEK/indexes/ --exclude "*" --include "part-00*"
+
 # make *.cdx.gz files (to be deleted later) private
-s3cmd setacl s3://commoncrawl/cc-index/CC-MAIN-$YEARWEEK/segments/ --acl-private --recursive
+#s3cmd setacl s3://commoncrawl/cc-index/CC-MAIN-$YEARWEEK/segments/ --acl-private --recursive
